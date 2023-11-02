1cff384fcab467d3bf0d9e2d29ea513c9fb68042
bonganimasemola <bonganimasemola@icloud.com>
2023-11-02 12:24:57 +0300

    minor edits

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -9,8 +9,6 @@ function MoviesPage({ movies }) {
   return (
     <div>
       <MoviesList movies={movies} />
-
-      {/* Adding code to show a message to the user to select a movie if they haven't yet */}
       <Route exact path={match.url}>
         <h3>Choose a movie from the list above</h3>
       </Route>
